095776804a0448b56dcd52aa97b2426a13895b93
Kwesi Phillips <alkwphillips@Kwesis-MacBook-Pro.local>
2014-09-20 15:55:20 -0700

    Fixing hover on .icon-design, code, fun

--- js/index.js
+++ js/index.js
@@ -5,18 +5,21 @@ $(document).ready(function() {
 	var intro = document.getElementById("intro-Text");
 	var hello = document.getElementById('hello');
 
-
 	$(currentTab).html("design");
 	$(".design").css("display", "block");
 
 	
 	// Skills Section Animations & Functions //
 	$("#skills").children().click(function (){
+		$(".icon").css("color", "#d9d9d9");
 		$(".content").css("display", "none");
+
 		var tabType = $(this).attr("data-tab-type");
-		console.log("MadeIt");
+		var currentColor = $(this).attr("data-tab-color");
+
 		$(currentTab).html(tabType);
 		$("."+tabType).css("display", "block");
+		$(".icon-"+tabType).css("color", currentColor);
 	});
 
 });
